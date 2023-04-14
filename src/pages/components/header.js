efbc4ce852c11d197535867728ec554c7a6223c2
gweryf <priyam2007sp@gmail.com>
2023-04-14 15:24:38 +0530

    addded header

---
+++ src/pages/components/header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <>
+            <div className=" bg-slate-400 shadow-lg text-center">
+                <p className=" text-2xl">Memory-Card</p>
+            </div>
+        </>
+    )
+}
