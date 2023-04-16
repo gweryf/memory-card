5414a7734c50389e97080b92783fac3a58ca51d3
gweryf <priyam2007sp@gmail.com>
2023-04-16 09:35:55 +0530

    made the header responsive

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -2,10 +2,12 @@ export default function Header(props) {
     const {score, highscore} = props
     return(
         <>
-            <div className=" bg-black shadow-lg text-center p-5">
+            <div className=" bg-black shadow-lg text-center p-5 md:flex justify-around">
                 <p className=" text-white font-bold text-4xl">Memory-Card</p>
+                <div>
                 <div className=" text-white font-bold text-lg">Score: {score}</div>
                 <div className=" text-white font-bold text-lg">Highscore: {highscore}</div>
+                </div>
             </div>
         </>
     )
